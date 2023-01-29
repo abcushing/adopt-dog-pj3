4c34f08d833ec1167ce840658b39e962db0d09d5
abcushing <abcushing789@gmail.com>
2023-01-29 13:12:12 -0700

    started making pages
    created home and about pages, re-ordered files

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import Home from "./pages/home";
+import About from "./pages/about";
+
+function App() {
+  return <About></About>;
+}
+
+export default App;
---
+++ client/src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <Header></Header>
+      contact here
+    </div>
+  );
+}
+
+export default Contact;
