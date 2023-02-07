a016b166aae6b17282855c0483d4563b1795c95f
abcushing <abcushing789@gmail.com>
2023-02-06 22:33:38 -0700

    port number
    port number issue

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,33 @@ import "./App.css";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import SearchResults from "./components/SearchResults";
 import Body from "./components/Body";
-import NavBar from "./components/navbar"
+import NavBar from "./components/navbar";
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: `http://localhost:${process.env.PORT}/graphql`,
   cache: new InMemoryCache(),
 });
 
 function App() {
-  const[current,setCurrent] = useState("search")
-  const [breed,setBreed] = useState('Saluki')
-
+  const [current, setCurrent] = useState("search");
+  const [breed, setBreed] = useState("Saluki");
 
   return (
     <>
-    <BrowserRouter>
-      <ApolloProvider client={client}>
-        <section >
-          <NavBar setCurrent={setCurrent} setBreed = {setBreed}/>
-          <Body current = {current} setCurrent= {setCurrent} breed = {breed} setBreed= {setBreed} />
-        </section>
-      </ApolloProvider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <ApolloProvider client={client}>
+          <section>
+            <NavBar setCurrent={setCurrent} setBreed={setBreed} />
+            <Body
+              current={current}
+              setCurrent={setCurrent}
+              breed={breed}
+              setBreed={setBreed}
+            />
+          </section>
+        </ApolloProvider>
+      </BrowserRouter>
     </>
   );
 }
